838daa03c77b2677882288e02429bd92ccaeeb79
Jeff Linwood <jlinwood@gmail.com>
2022-07-21 16:08:09 -0500

    Initial Commit

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "<h1>Hello</h1>"
+
+@app.get("/cats")
+def cats():
+    all_cats = get_cats()
+    return { "cats": all_cats }
+
+def get_cats():
+    return ["Tabby", "Tuxedo Cat", "Norwegian Forest Cats"]
